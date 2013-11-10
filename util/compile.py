98e4903f657f65bf89cdc5e5a96c75c8e564639a
Alex Yatskov <alex@foosoft.net>
2013-11-09 19:12:17 -0800

    Fixing up headers

--- util/compile.py
+++ util/compile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-#
 # Copyright (C) 2013  Alex Yatskov
 #
 # This program is free software: you can redistribute it and/or modify
@@ -15,7 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 
 import codecs
