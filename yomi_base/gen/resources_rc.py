6b63422efb408ed77b2a41708a7b9aaac3bed269
Alex Yatskov <alex@foosoft.net>
2013-11-11 15:16:41 -0800

    Updating UI for preference dialog
    Former-commit-id: d5e16b08b55ef474e94fa5d94f5f1f486befc418

--- yomi_base/gen/resources_rc.py
+++ yomi_base/gen/resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Sun Nov 10 17:56:51 2013
+# Created: Mon Nov 11 15:13:30 2013
 #      by: The Resource Compiler for PyQt (Qt v4.8.4)
 #
 # WARNING! All changes made in this file will be lost!
