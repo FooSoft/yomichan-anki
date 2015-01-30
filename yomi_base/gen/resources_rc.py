63ba3bb6d41fbff3503a8f97a9adda41117cf09c
Alex Yatskov <alex@foosoft.net>
2015-01-30 18:38:01 +0900

    Add checkbox to allow editing the main text area
    Former-commit-id: 67061d862173e3acd4c5b7eea9a5a62d84b1b6ba

--- yomi_base/gen/resources_rc.py
+++ yomi_base/gen/resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Wed Dec 24 17:04:45 2014
+# Created: Fri Jan 30 18:21:34 2015
 #      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
